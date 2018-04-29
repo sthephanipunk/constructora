1c87f5a7b6f4a93549185f0d59c5898e91259b37
sthephanipunk <stephaniolvera59@gmail.com>
2018-04-29 09:10:48 -0500

    cambios y comentarios en index y nuevo estilo-login

--- index.php
+++ index.php
@@ -1,39 +1,47 @@
  <!DOCTYPE html>
 <html >
 <head>
-   <script src="javascript/Live.js" name="para recargar los cambios de css y javascript sin problemas"></script>
+     <script src="javascript/Live.js" name="para recargar los cambios de css y javascript sin problemas"></script>
      <meta charset="UTF-8">
-  <!--<meta name="viewport" content="width=device-width, initial-scale=1.0" />-->
-      <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-
-  <meta charset="utf-8">
-  <title>Construcciones</title>
-      <link rel="stylesheet" href="css/estilou.css">
+ <!--<meta name="viewport" content="width=device-width, initial-scale=1.0" />-->
+     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+     <title>Construcciones</title>
+     <link rel="stylesheet" href="css/estilo-login.css">
 </head>
 
 <body>
-  <div id="particles-js"></div>
-    <br><br><br><br>
+     <!--<div id="particles-js"></div>-->
+       <br><br><br><br>
+        <!--<div class="container">-->
+	          
 
-  <div class="container">
-	<section id="content">
-		<form action="validar.php" name="login" method="post">
-			<img src="imagenes\usua.png" width="100px" height="100px"> 
-			   <h1>Iniciar sesión</h1>
-			   <div>
-				<input type="text" placeholder="Id_Usuario" required="" id="username" name="id"/><br><br>
-			   </div>
+	          <!--  <section id="content">-->
+		           
+		             <form action="validar.php" name="login" method="post">
+		    
+		              	
+			          <!--<img src="imagenes\usua.png" >-->
+			       
+			   
+			          <header>
+			          <h1>Iniciar sesión</h1>
+			          </header>
+			   
+			          <div>
+				      <input type="text" placeholder="Id_Usuario" required="" id="username" name="id"/><br><br>
+			          </div>
 
-			   <div>
-				<input type="password" placeholder="Password" required="" id="password" name="contra" />
-			   </div>
+			          <div>
+				      <input type="password" placeholder="Password" required="" id="password" name="contra" />
+			          </div>
 
-		       <div>
-				<input type="submit" value="aceptar" name="btn" />
-			 </div>
-		</form>
-	  </section>
-   </div>
+		              <div>
+				      <input type="submit" value="Aceptar" name="btn" />
+			          </div>
+		              </form>
+	       <!--  </section>-->
+	         
+        <!--</div>-->
 
 
 <script src="javascript/estiloparticulas.js"></script>
